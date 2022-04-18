1d89fe15d1f90a6f0afdfb1804201f97774b462d
brunodmartins <bdm2943@gmail.com>
2022-04-18 18:09:45 -0300

    More Swagger docs

--- src/main/kotlin/com/github/brunodmartins/opengymapi/core/student/StudentController.kt
+++ src/main/kotlin/com/github/brunodmartins/opengymapi/core/student/StudentController.kt
@@ -2,12 +2,15 @@ package com.github.brunodmartins.opengymapi.core.student
 
 import com.github.brunodmartins.opengymapi.core.domain.Student
 import com.github.brunodmartins.opengymapi.core.domain.dto.api.StudentRequest
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiOperation
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import javax.validation.Valid
 
+@Api(tags = ["Student API"])
 @RestController
 @RequestMapping("/student")
 class StudentController {
@@ -15,6 +18,7 @@ class StudentController {
     @Autowired
     lateinit var service: StudentService
 
+    @ApiOperation("Create student")
     @PostMapping
     fun postStudent(@Valid @RequestBody request: StudentRequest): ResponseEntity<Student> {
         val student = request.toStudent()
@@ -22,6 +26,7 @@ class StudentController {
         return ResponseEntity<Student>(student, HttpStatus.CREATED)
     }
 
+    @ApiOperation("Get a student by ID")
     @GetMapping("/{id}")
     fun getStudent(@PathVariable id: Long): ResponseEntity<Student> {
         return ResponseEntity<Student>(service.get(id), HttpStatus.OK)
